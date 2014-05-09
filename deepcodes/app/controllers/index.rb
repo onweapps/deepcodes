6a0157d178620e6d2af74eba65ec970d90a0dca1
Keaty & Adam Godel <pair+keaty.onweapps@devbootcamp.com>
2014-05-09 16:13:46 -0400

    merged Sinatra models, tables and views with original JavaScript

--- deepcodes/app/controllers/index.rb
+++ deepcodes/app/controllers/index.rb
@@ -1,4 +1,11 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/games' do
+  erb :games
+end
+
+get '/dual_n_back' do
+  erb :dual_n_back
 end
--- deepcodes/public/js/application.js
+++ deepcodes/public/js/application.js
@@ -1,7 +1,55 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+$(document).ready(function () {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#begin').click(function(event) {
+    Game.init(2);
+    Game.run();
+  });
+
+  $("html").keypress(function() {
+    if (event.keyCode === 97) {
+      Game.check_audio();
+    } else if (event.keyCode === 108) {
+      Game.check_visual();
+    } else {
+      console.log("nope");
+    };
+  });
+
+  var Game = {
+    init: function(n) {
+      this.n = n,
+      this.turns = []
+    },
+    run: function() {
+      var progress = 99
+      function repeatOften() {
+        progress += 1
+        if (progress % 120 === 0) {
+          var randCell = Math.floor(Math.random()*9)+1;
+          Game.turns.push(new Turn(randCell));
+          $("#grid div").css("background-color", "#fff");
+          $('#grid div:nth-child(' + randCell + ')').css({'background-color': '#7f8c8d'});
+        }
+        if (progress < 1200) { requestAnimationFrame(repeatOften); }
+      };
+      requestAnimationFrame(repeatOften);
+    },
+    check_visual: function() {
+      if(this.turns[this.turns.length-1].visual === this.turns[this.turns.length-this.n-1].visual){
+        console.log("correct")
+      } else {
+        console.log("incorrect")
+      }
+    },
+    check_audio: function() {
+      console.log("audio");
+      // match if Game.turns.last.audio = Game.turns[n-1].audio
+    }
+  };
+
+  var Turn = function(visual) {
+    this.visual = visual
+    //this.letter = letter
+  };
+
 });
